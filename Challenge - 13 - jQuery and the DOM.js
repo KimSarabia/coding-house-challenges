41fc16070eb3caff5cd24466f10ed456aa96cace
Kim Sarabia <kimberly.sarabia@gmail.com>
2015-12-23 03:47:50 -0600

    Starting Challenge 13
    Putting in the instructions for clarity

---
+++ Challenge - 13 - jQuery and the DOM.js
@@ -0,0 +1,17 @@
+//Write jQuery code to loop over every DIV in any HTML document and give that DIV a random color background. 
+//Then invert the DIVs background color using the function below, and use the inverted color for the font on that DIV.
+
+//Test your code in Chrome console on many pages.
+
+//Here's the function to use for inverting colors:
+
+function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
