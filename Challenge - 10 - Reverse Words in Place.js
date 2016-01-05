14ff5b20fa18c229e543ec59f4830e96ce1b766a
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-01-05 12:51:13 -0600

    Latest
    Switched folders

--- Challenge - 10 - Reverse Words in Place.js
+++
@@ -1,5 +0,0 @@
-function reverse(string) {
-  for (var i = string.length - 1, o = ''; i >= 0; o += string[i--]) { }
-  return o;
-}
-console.log(reverse("I love Coding House!"));
--- README.md
+++
@@ -1,2 +0,0 @@
-# coding-house-challenges
-Coding House Challenges 
